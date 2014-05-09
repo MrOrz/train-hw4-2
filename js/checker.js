6afdb6cbcd1c4a1467797c4062485c9cb9965e15
MrOrz <johnsonliang7@gmail.com>
2014-05-09 18:14:20 +0800

    Change to "Friend's photo with most likes."

--- js/checker.js
+++ js/checker.js
@@ -3,24 +3,6 @@
 var startButton = $('.hw4-start-button'), // 「開始掃描」按鈕
     results = $('.hw4-result'); // 「掃描結果」 table
 
-// 垃圾社團列表
-var junkGroups = [];
-
-// 用 Ajax 自 http://spamgroup.tonyq.org/groups/jsonp 取得垃圾社團列表
-
-$.ajax('http://spamgroup.tonyq.org/groups/jsonp', {
-  dataType: 'jsonp',
-  jsonp: 'jsonp',
-  success: function(data){
-    // 將每筆資料的 GID 放進 junkGroups 陣列中。
-    data.forEach(function(record){
-      junkGroups.push(record.GID);
-    });
-
-    startButton.removeAttr('disabled').removeClass('disabled');
-  }
-});
-
 // 設定 Facebook AppID
 window.fbAsyncInit = function(){
   FB.init({
@@ -38,18 +20,29 @@ window.fbAsyncInit = function(){
     FB.login(function(){
       // 2. 以 FB.api 拿到使用者的 group 列表
       // 拿到使用者 group 列表的 response 之後：
-      FB.api('/me/groups', function(resp){
+      FB.api('/me/friends', function(friends){
         var i;
-        for(i=0; i<resp.data.length; i+=1){
-          if( junkGroups.indexOf( resp.data[i].id ) !== -1 ){
-            // resp.data[i] 是垃圾社團！
-            // 在表格裡面顯示
-            results.append('<tr><td>'+resp.data[i].id+'</td><td>'+resp.data[i].name+'</td></tr>');
-          }
+        for(i=0; i<friends.data.length; i+=1){
+
+          FB.api('/' + friends.data[i].id + '/albums', function(friendAlbums){
+            var i;
+            for(i=0; i<friendAlbums.data.length; i+=1){
+              FB.api('/' + friendAlbums.data[i].id + '/photos', function(photos){
+                var i, maxPhoto, maxCount = 0;
+                for(i=0; i<photos.data.length; i+=1){
+                  if(maxCount < photos.data[i].likes.data.length){
+                    maxCount = photos.data[i].likes.data.length;
+                    maxPhoto = photos.data[i];
+                  }
+                }
+                results.append('<div class="col-md-3"><figure><a href="'+maxPhoto.link+'"><img src="'+maxPhoto.picture+'"></a><figcaption>'+maxPhoto.from.name+'</figcaption></div>');
+              });
+            }
+          });
         }
         results.after('<div class="hw4-complete alert alert-info">掃描完成</div>');
       });
-    }, {scope: 'user_groups'});
+    }, {scope: 'user_friends,friends_photos'});
 
   });
 };
